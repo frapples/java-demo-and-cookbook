6addd80f1428d1083835ded9d5263b5496052165
Frapples <isfrapples@outlook.com>
2018-02-05 19:41:58 +0800

    添加上传文件代码

---
+++ src/main/java/io/github/frapples/springbootcookbook/biz/ResponseDTO.java
@@ -0,0 +1,27 @@
+package io.github.frapples.springbootcookbook.biz;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/*
+* 1. 这是一个POJO类。要注意的一点是，如jackjson库序列化时，是需要getter的。
+ *  */
+@AllArgsConstructor
+@Getter
+public class ResponseDTO<T> {
+
+    public static final int SUCCESS_CODE = 200;
+    public static final int FAILED_CODE = 500;
+
+    private Integer code;
+    private String msg;
+    private T data;
+
+    public static <T> ResponseDTO<T> ofSuccess(T data) {
+        return new ResponseDTO<T>(SUCCESS_CODE, "执行成功", data);
+    }
+
+    public static <T> ResponseDTO<T> ofFailed() {
+        return new ResponseDTO<T>(FAILED_CODE, "系统错误", null);
+    }
+}
--- src/main/java/io/github/frapples/springbootcookbook/web/controller/HelloController.java
+++ src/main/java/io/github/frapples/springbootcookbook/web/controller/HelloController.java
@@ -1,11 +1,16 @@
 package io.github.frapples.springbootcookbook.web.controller;
 
+import io.github.frapples.springbootcookbook.biz.ResponseDTO;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/api")
@@ -18,5 +23,35 @@ public class HelloController {
         map.put("a", "1");
         map.put("b", "2");
         return map;
+    }
+
+    /*
+    * 1. 演示了如何进行文件上传
+    * */
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseDTO upload(@RequestParam("file") MultipartFile file,
+        @RequestParam("helloId") String helloId) {
+        if (file.isEmpty()) {
+            return ResponseDTO.ofFailed();
+        }
+
+        String fileName = file.getOriginalFilename();
+        String ext = fileName.substring(fileName.lastIndexOf("."));
+
+        try {
+            File tempFile = File.createTempFile("tmp", null);
+            file.transferTo(tempFile);
+            tempFile.deleteOnExit();
+            // 其它操作
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("fileName", fileName);
+            map.put("ext", ext);
+            return ResponseDTO.ofSuccess(map);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseDTO.ofFailed();
+        }
     }
 }
