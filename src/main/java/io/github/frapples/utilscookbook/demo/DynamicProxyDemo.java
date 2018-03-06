b869fba98c2593f2102bb2d44ed93913e7dbf570
Frapples <isfrapples@outlook.com>
2018-03-06 17:02:49 +0800

    添加动态代理demo

---
+++ src/main/java/io/github/frapples/utilscookbook/demo/DynamicProxyDemo.java
@@ -0,0 +1,62 @@
+package io.github.frapples.utilscookbook.demo;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/*
+* 1. 这是java实现动态代理的小demo. https://troywu0.gitbooks.io/spark/content/%E5%8A%A8%E6%80%81%E4%BB%A3%E7%90%86.html
+* 2. https://www.zhihu.com/question/20794107
+*
+* 作用:
+* 1. 动态代理. 虽然叫动态代理,其实更像动态装饰器. 在InvocationHandler接口的invoke方法中, 你可以控制代理方法中执行的代码
+* 1. 传统静态代理写法要手动的把一个一个的函数代理出去, 代码量和工作量随业务庞大而庞大. 动态代理就能解决此问题
+* 1. 你可以在invoke方法中, 执行被代理对象的方法前后执行额外的代码. 这就是实现AOP
+* 1. 可你可以在invoke方法中, 通过通讯网络获取数据以实现对象行为, 这就是实现了远程调用
+* */
+
+public class DynamicProxyDemo {
+
+    public static void main(String[] args) {
+        NumerOperationInterface real = new RealObject();
+        real = RealObjectProxyHandler.wrap(real);
+        real.add(1, 2);
+    }
+}
+
+
+class RealObjectProxyHandler implements InvocationHandler {
+
+    private Object proxied;
+
+    private RealObjectProxyHandler(Object proxied) {
+        this.proxied = proxied;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.printf("调用函数%s\n", method.getName());
+        return method.invoke(proxied, args);
+    }
+
+    static public NumerOperationInterface wrap(NumerOperationInterface real) {
+        /* 这个函数只能传入接口, 因此被代理类需要实现接口 */
+        return (NumerOperationInterface) Proxy.newProxyInstance(
+            NumerOperationInterface.class.getClassLoader(),
+            new Class[]{NumerOperationInterface.class},
+            new RealObjectProxyHandler(real));
+    }
+}
+
+interface NumerOperationInterface {
+    int add(int a, int b);
+}
+
+class RealObject implements NumerOperationInterface {
+
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
